c8e19e79a1c91aaaac8e2d9cf6f44e416dc72c2d
Arash alone boy <titanteam0098@gmail.com>
2016-03-30 20:32:19 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,31 @@
+do
+
+function run(msg, matches)
+  return 'WALLE-BOT v1'.. VERSION .. [[ 
+ 
+  Developer : @dev_boy
+  
+  Channel : @CopierTeam
+  
+  Open git : https://github.com/Arashalone/wall-e.git
+  
+  WALL-E VERSION 1
+  
+  All rights reserved.
+  __________________]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
